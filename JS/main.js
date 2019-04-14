3a1ec62fc9b5c93df8057b41dec21a0056012f63
Artyom <artyom.zayarny@gmail.com>
2019-04-14 23:02:50 +0300

    Responsive reasons section + sticky top navbar

--- JS/main.js
+++ JS/main.js
@@ -8,4 +8,20 @@ $(document).ready(function(){
         centerMode: true,
         variableWidth: true
     });
+
+    window.onscroll = function() {myFunction()};
+
+    var navbar = document.getElementById("header-nav");
+    var logo = document.getElementById("logo")
+    var sticky = navbar.offsetTop;
+
+    function myFunction() {
+        if (window.pageYOffset >= sticky) {
+            navbar.classList.add("sticky")
+            logo.classList.add('stuck');
+        } else {
+            navbar.classList.remove("sticky");
+            logo.classList.remove('stuck');
+        }
+    }
 });

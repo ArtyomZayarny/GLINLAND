d67575a6bd297ad4dcff76d32d665efa99b9eb0b
Artyom <artyom.zayarny@gmail.com>
2019-04-17 00:16:02 +0300

    Section additional css

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,18 @@
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("header-nav");
+var logo = document.getElementById("logo")
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+        logo.classList.add('stuck');
+    } else {
+        navbar.classList.remove("sticky");
+        logo.classList.remove('stuck');
+    }
+}
 $(document).ready(function(){
 
     $('.events_slider').slick({
@@ -8,20 +23,23 @@ $(document).ready(function(){
         centerMode: true,
         variableWidth: true
     });
+    $('.studio_slider').slick({
+        dots:true,
+        centerMode: true,
+        slidesToShow: 3,
+        arrows: true,
+        variableWidth: true,
 
-    window.onscroll = function() {myFunction()};
-
-    var navbar = document.getElementById("header-nav");
-    var logo = document.getElementById("logo")
-    var sticky = navbar.offsetTop;
-
-    function myFunction() {
-        if (window.pageYOffset >= sticky) {
-            navbar.classList.add("sticky")
-            logo.classList.add('stuck');
-        } else {
-            navbar.classList.remove("sticky");
-            logo.classList.remove('stuck');
-        }
-    }
+    });
+    //additional_slider
+    // $('.additional_slider').slick({
+    //     centerMode:true,
+    //     slidesToShow: 1,
+    //     slidesToScroll: 1,
+    //     dots: true,
+    //     infinite: true,
+    //     cssEase: 'linear',
+    //     variableWidth: true,
+    //     variableHeight: true
+    // });
 });

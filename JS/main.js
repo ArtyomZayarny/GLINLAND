8cbb085361bd9b0c88ae15900cb9a20f3a491be4
unknown <artyom.zayarny@gmail.com>
2019-04-21 21:23:11 +0300

    responsive contact form and gallery

--- JS/main.js
+++ JS/main.js
@@ -15,33 +15,33 @@ function myFunction() {
 }
 $(document).ready(function(){
 
-    $('.events_slider').slick({
-        dots: true,
-        infinite: true,
-        speed: 300,
-        slidesToShow: 1,
-        centerMode: true,
-        variableWidth: true
-    });
-    $('.studio_slider').slick({
-        dots:true,
-        centerMode: true,
-        slidesToShow: 3,
-        arrows: true,
-        variableWidth: true,
-
-    });
-    //additional_slider
-    // $('.additional_slider').slick({
-    //     centerMode:true,
-    //     slidesToShow: 1,
-    //     slidesToScroll: 1,
+    // $('.events_slider').slick({
     //     dots: true,
     //     infinite: true,
-    //     cssEase: 'linear',
-    //     variableWidth: true,
-    //     variableHeight: true
+    //     speed: 300,
+    //     slidesToShow: 1,
+    //     centerMode: true,
+    //     variableWidth: true
     // });
+    // $('.studio_slider').slick({
+    //     dots:true,
+    //     centerMode: true,
+    //     slidesToShow: 3,
+    //     arrows: true,
+    //     variableWidth: true,
+    //
+    // });
+    // //additional_slider
+    // // $('.additional_slider').slick({
+    // //     centerMode:true,
+    // //     slidesToShow: 1,
+    // //     slidesToScroll: 1,
+    // //     dots: true,
+    // //     infinite: true,
+    // //     cssEase: 'linear',
+    // //     variableWidth: true,
+    // //     variableHeight: true
+    // // });
 
 
     $( "#datepicker" ).datepicker({
